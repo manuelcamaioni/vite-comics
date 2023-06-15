6fab3a0dea081da4312da6040b44053de453189c
manuelcamaioni <camaioni.manuel@gmail.com>
2023-06-15 15:09:37 +0200

    imported imgs ion assets and implemented appheader component

--- src/App.vue
+++ src/App.vue
@@ -20,4 +20,6 @@ export default {
 };
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+@import "./styles/general.scss";
+</style>
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,13 +1,74 @@
 <template>
-    <h1>Header</h1>
+    <header>
+        <div class="container">
+            <nav class="d-flex">
+                <div class="img-container">
+                    <img
+                        src="../assets/vue-dc-comics-1/img/dc-logo.png"
+                        alt="" />
+                </div>
+                <div class="list-container d-flex">
+                    <ul class="d-flex">
+                        <li v-for="link in links">{{ link.name }}</li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    </header>
 </template>
 
 <script>
 export default {
     data() {
-        return {};
+        return {
+            links: [
+                {
+                    name: "Characters",
+                },
+                {
+                    name: "Comics",
+                },
+                {
+                    name: "Movies",
+                },
+                {
+                    name: "TV",
+                },
+                {
+                    name: "Games",
+                },
+                {
+                    name: "Collectibles",
+                },
+                {
+                    name: "Videos",
+                },
+                {
+                    name: "Fans",
+                },
+                {
+                    name: "News",
+                },
+                {
+                    name: "Shop",
+                },
+            ],
+        };
     },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+nav {
+    justify-content: space-between;
+    .list-container {
+        align-items: center;
+        ul {
+            li {
+                margin: 0.5rem 0.75rem;
+                text-transform: uppercase;
+            }
+        }
+    }
+}
+</style>
