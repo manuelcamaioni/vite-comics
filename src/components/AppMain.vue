5d1907b3e3abea13606c5c42510be375ed2c3385
manuelcamaioni <camaioni.manuel@gmail.com>
2023-06-15 15:26:24 +0200

    implemented main SFC

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,5 +1,9 @@
 <template>
-    <h1>Main</h1>
+    <main>
+        <div class="container">
+            <h1>--> Content goes here &lt;--</h1>
+        </div>
+    </main>
 </template>
 
 <script>
@@ -10,4 +14,12 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+main {
+    background-color: #1c1c1c;
+    color: white;
+    h1 {
+        padding: 3rem 0;
+    }
+}
+</style>
